14406e8091e74fd22065c73b59fee5b440209299
lcestarolli <l.cestarolli@gmail.com>
2019-08-15 11:03:11 -0300

    more challenges

---
+++ problem-solving/MagicSquare.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class MagicSquare {
+
+    // Complete the formingMagicSquare function below.
+    static int formingMagicSquare(int[][] s) {
+
+
+        for (int i = 0; i < s.length; i++) {
+            int row = 0;
+            for (int j = 0; j < s[i].length; j++) {
+                row += s[i][j];
+            }
+            System.out.println(row);
+        }
+
+        return 0;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("OUTPUT_PATH"));
+
+        int[][] s = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            String[] sRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int sItem = Integer.parseInt(sRowItems[j]);
+                s[i][j] = sItem;
+            }
+        }
+
+        int result = formingMagicSquare(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
