7a80c0a5db4bb8013077d739ce2102688f115bea
lcestarolli <l.cestarolli@gmail.com>
2019-09-03 11:03:30 -0300

    more challenges

---
+++ problem-solving/Regex.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Regex {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        List<String> names = new ArrayList<String>();
+
+        for (int NItr = 0; NItr < N; NItr++) {
+            String[] firstNameEmailID = scanner.nextLine().split(" ");
+
+            String firstName = firstNameEmailID[0];
+
+            String emailID = firstNameEmailID[1];
+
+            if(emailID.matches(".*@gmail.com")){
+                names.add(firstName);
+            }
+        }
+
+        Collections.sort(names);
+        for (String name: names) {
+            System.out.println(name);
+        }
+
+        scanner.close();
+    }
+}
